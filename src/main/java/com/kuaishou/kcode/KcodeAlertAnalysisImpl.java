fbf8ed5089c93911f665d2051aa433289b28a7b0
ayang818 <ayang818@qq.com>
2020-07-15 23:34:51 +0800

    线程调整小一点

--- src/main/java/com/kuaishou/kcode/KcodeAlertAnalysisImpl.java
+++ src/main/java/com/kuaishou/kcode/KcodeAlertAnalysisImpl.java
@@ -26,22 +26,17 @@ public class KcodeAlertAnalysisImpl implements KcodeAlertAnalysis {
     int maxCount = 5000;
     Semaphore count = new Semaphore(maxCount);
     /* 数据处理线程池 */
-    ThreadPoolExecutor threadPool = new ThreadPoolExecutor(16, 16, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<>(maxCount));
+    ThreadPoolExecutor threadPool = new ThreadPoolExecutor(4, 4, 60, TimeUnit.SECONDS, new LinkedBlockingQueue<>(maxCount));
     /* thread safe formatter */
     /* global date formatter */
     SimpleDateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd HH:mm");
     /* 一个线程池中的任务多少行 */
-    int taskNumberThreshold = 1000;
+    int taskNumberThreshold = 2000;
     /* 每分钟的毫秒跨度 */
     int millspace = 60000;
 
     @Override
     public Collection<String> alarmMonitor(String path, Collection<String> alertRules) {
-        // try {
-        //     Files.lines(Paths.get(path)).parallel().forEach(this::handleLine);
-        // } catch (IOException e) {
-        //     e.printStackTrace();
-        // }
         BufferedReader bufferedReader = null;
         String line;
         try {
